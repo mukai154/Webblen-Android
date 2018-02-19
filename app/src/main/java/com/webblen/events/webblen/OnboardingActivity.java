997e301222b73fa1338b360e2641741ad70227e2
mukai154 <mukaiss@outlook.com>
2018-02-19 09:08:33 -0600

    onboarding

--- app/src/main/java/com/webblen/events/webblen/OnboardingActivity.java
+++ app/src/main/java/com/webblen/events/webblen/OnboardingActivity.java
@@ -1,13 +1,22 @@
 package com.webblen.events.webblen;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class OnboardingActivity extends AppCompatActivity {
+
+    private ViewPager slideViewPager;
+    private LinearLayout dotsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        slideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        dotsLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
     }
 }
