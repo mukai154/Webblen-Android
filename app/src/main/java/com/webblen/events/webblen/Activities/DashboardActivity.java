9c856fc29503600360d443a95f2d07547480611f
mukai154 <mukaiss@outlook.com>
2018-05-11 20:31:20 -0500

    Create New Dashboard

--- app/src/main/java/com/webblen/events/webblen/Activities/DashboardActivity.java
+++ app/src/main/java/com/webblen/events/webblen/Activities/DashboardActivity.java
@@ -6,17 +6,24 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.facebook.login.LoginManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.webblen.events.webblen.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -28,7 +35,7 @@ public class DashboardActivity extends AppCompatActivity {
     private FirebaseFirestore firebaseFirestore;
     private String user_id;
 
-    //Image & Name
+    //UI Elements
     private CircleImageView userWalletPic;
     private Uri mainImageURI = null;
     private TextView usernameWalletText;
@@ -38,6 +45,8 @@ public class DashboardActivity extends AppCompatActivity {
     private FrameLayout createEventFrame;
     private FrameLayout myEventsFrame;
     private FrameLayout walletFrame;
+    private Button logoutBtn;
+
 
 
     private ProgressBar walletProgress;
@@ -46,6 +55,17 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //Configure Twitter SDK
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(
+                getString(R.string.twitter_key),
+                getString(R.string.twitter_secret));
+
+        TwitterConfig twitterConfig = new TwitterConfig.Builder(this)
+                .twitterAuthConfig(authConfig)
+                .build();
+
+        Twitter.initialize(twitterConfig);
 
         //** INITIALIZE
 
@@ -64,6 +84,7 @@ public class DashboardActivity extends AppCompatActivity {
         createEventFrame = (FrameLayout) findViewById(R.id.createEventDashboardBtn);
         myEventsFrame = (FrameLayout) findViewById(R.id.myEventsDashboardBtn);
         walletFrame = (FrameLayout) findViewById(R.id.walletDashboardBtn);
+        logoutBtn = (Button) findViewById(R.id.logoutDashboardBtn);
 
         createEventFrame.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,16 +94,24 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                LoginManager.getInstance().logOut();
+                TwitterCore.getInstance().getSessionManager().clearActiveSession();
+                logoutUser();
+            }
+        });
+
         loadFirestoreData();
 
     }
 
     private void loadFirestoreData() {
-
         firebaseFirestore.collection("users").document(user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-
                 if(task.isSuccessful()){
 
                     if(task.getResult().exists()){
@@ -96,7 +125,6 @@ public class DashboardActivity extends AppCompatActivity {
                             startActivity(setupIntent);
                             finish();
                         } else {
-
                             mainImageURI = Uri.parse(profile_pic);
                             usernameWalletText.setText("@" + username);
 
@@ -105,19 +133,32 @@ public class DashboardActivity extends AppCompatActivity {
                             userWalletPic.setVisibility(View.VISIBLE);
                             usernameWalletText.setVisibility(View.VISIBLE);
                             walletProgress.setVisibility(View.INVISIBLE);
-
                         }
                     }
-
                 } else {
 
                     String error = task.getException().getMessage();
                     Toast.makeText(DashboardActivity.this, "Load Error: " + error, Toast.LENGTH_LONG).show();
-
                 }
-
             }
         });
+    }
+
+    //** Authentication Methods and Handling
+    @Override
+    public void onStart(){
+        //Check if Firebase user is signed in and act accordingly
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser == null){
+            logoutUser();
+        }
+
+    }
+    private void logoutUser(){
+        Intent logoutIntent = new Intent(DashboardActivity.this, LoginActivity.class);
+        startActivity(logoutIntent);
+        finish();
     }
 
 }
